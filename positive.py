e0d5594873ca9a03071b1394255d516314148da7
mayuri2404 <69312298+mayuri2404@users.noreply.github.com>
2020-08-06 21:06:15 +0530

    Created positive.py file
    It will print positive numbers from list

---
+++ positive.py
@@ -0,0 +1,15 @@
+list1 = [12,-7,5,64,-14]
+print("Positive numbers in list 1")
+for var in list1:
+    if var>0:
+        print(var)
+    else:
+        continue
+        
+list2 = [12,14,-95,3]
+print("Positive numbers in list 2")
+for var in list2:
+    if var>0:
+        print(var)
+    else:
+        continue
